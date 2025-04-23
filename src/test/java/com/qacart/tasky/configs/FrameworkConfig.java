a2cbb670ffe831131f63f6a4f18400f6f8dcf2ef
Tamer Alsharawneh <tameralsharawneh12@gmail.com>
2025-04-23 20:21:35 +0300

    Prepare driver selection, and another configuration such as urls and access_token

---
+++ src/test/java/com/qacart/tasky/configs/FrameworkConfig.java
@@ -0,0 +1,24 @@
+package com.qacart.tasky.configs;
+
+
+import com.qacart.tasky.converter.ConvertStringToEnumType;
+import com.qacart.tasky.enums.BrowserType;
+import org.aeonbits.owner.Config;
+
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({"system:env", "system:properties", "file:${user.dir}/src/test/resources/config.properties"})
+public interface FrameworkConfig extends Config {
+
+    @ConverterClass(ConvertStringToEnumType.class)
+    @DefaultValue("CHROME")
+    BrowserType browser();
+
+    @DefaultValue("https://tasky-be.qacart.com/api")
+    @Key("api.url")
+    String apiURL();
+
+    @DefaultValue("https://tasky.qacart.com")
+    @Key("pageBase.url")
+    String pageBaseURL();
+}
